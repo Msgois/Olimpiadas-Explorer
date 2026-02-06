7a6f770112a76e2357a74c82b9eae5294234a91b
José Alexandre Gonçalves Dantas <alexandre.dantas@dcomp.ufs.br>
2026-02-05 23:09:52 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "RakingDosPaisesPorEsporte"
+#include "RakingDosPaisesPorEsporte.c"
 #include "paises_com_mais_participacao.c"
 #include "Top_Dez_medalhistas.c"
 #include "evolucao_mulheres_olimpiadas.c"
